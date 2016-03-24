6d913be086e1a6913bdf20251a8c16f53b235e3e
Michael Hari <mdhari@users.noreply.github.com>
2016-03-23 23:45:47 -0700

    Added orange background/white text on HRA screen

--- SAHC/SAHC/SAHCTabBarController.swift
+++ SAHC/SAHC/SAHCTabBarController.swift
@@ -13,8 +13,24 @@ class SAHCTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // http://stackoverflow.com/questions/30041127/ios-8-tab-bar-item-background-colour
+        // add the white selection and orange 
+        UITabBar.appearance().tintColor = UIColor.whiteColor()
+        UITabBar.appearance().selectionIndicatorImage = UIImage().makeImageWithColorAndSize(UIColor(red: 245.0/255, green: 132.0/255, blue: 30.0/255, alpha: 1.0), size: CGSizeMake(tabBar.frame.width/4, tabBar.frame.height))
         // default to second tab "HRA"
         self.selectedIndex = 1
     }
     
+}
+
+// http://stackoverflow.com/questions/30041127/ios-8-tab-bar-item-background-colour
+extension UIImage {
+    func makeImageWithColorAndSize(color: UIColor, size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        color.setFill()
+        UIRectFill(CGRectMake(0, 0, size.width, size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
 }
