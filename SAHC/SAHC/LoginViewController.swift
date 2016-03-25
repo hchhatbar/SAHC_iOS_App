7ee9ed93a6dd678352d21a1d9c2fc2c5c622fc5d
Michael Hari <mdhari@users.noreply.github.com>
2016-03-24 21:09:52 -0700

    Basic Login View Controller based on new specs

---
+++ SAHC/SAHC/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  SAHC
+//
+//  Created by Michael Hari on 3/24/16.
+//  Copyright © 2016 AppForCause. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
